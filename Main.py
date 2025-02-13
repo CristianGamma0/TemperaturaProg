be17e7334e1cf19508d79a3bc447846bd3fc9e85
CristianGamma0 <cristiansottile07@gmail.com>
2025-02-13 11:38:06 +0100

    Sensore e app

--- Main.py
+++ Main.py
@@ -14,6 +14,8 @@ def funzione_thread():
 
         print(dataT, '°C')
         print(dataU, '%')
+        dpg.set_value("Temp", dataT)
+        dpg.set_value("Umi", dataU)
 
 
 # Creazione del thread
@@ -22,13 +24,17 @@ thread = threading.Thread(target=funzione_thread)
 # Avvio del thread
 thread.start()
 
-
 dpg.create_context()
 
-with dpg.window(label="Temperatura"):
-    dpg.add_text("sium")
-
 dpg.create_viewport(title="Bomboclat", width=400, height=300)
+with dpg.window(label="Temperatura"):
+    with dpg.group(horizontal=True):
+        dpg.add_text(tag="Temp")
+        dpg.add_text('°C')
+    with dpg.group(horizontal=True):
+        dpg.add_text(tag="Umi")
+        dpg.add_text('%')
+
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()

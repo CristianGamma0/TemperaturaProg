bfa2b0ebf5dbe6c7d43648f5786bc05596fa5e0b
CristianGamma0 <cristiansottile07@gmail.com>
2025-03-06 12:06:12 +0100

    Sensore e app

--- Main.py
+++ Main.py
@@ -4,7 +4,10 @@ from datetime import datetime
 import threading
 import time
 import pytz  # Ensure proper timezone handling
+import json
 
+with open("Dati.json", "w") as f:
+    json.dump("0", f)
 # Set your local timezone (change "Europe/Rome" if needed)
 local_timezone = pytz.timezone("Europe/Rome")
 
@@ -14,7 +17,7 @@ time_list = []  # Lista per i timestamp (asse x)
 
 
 def funzione_thread():
-    ser = serial.Serial("COM15")
+    ser = serial.Serial("COM7")
 
     while True:
         data = ser.readline().decode("utf-8")
@@ -28,7 +31,7 @@ def funzione_thread():
         dpg.set_value("Temp", f"{dataT} °C")
         dpg.set_value("Umi", f"{dataU} %")
 
-        # Ottieni l'orario attuale come timestamp (secondi dall'epoch)
+        # Ottieni l'orario attuale come timestamp
         current_time = datetime.now(local_timezone)
         # Add the offset (in seconds) to convert the UTC timestamp to local time
         timestamp = current_time.timestamp() + current_time.utcoffset().total_seconds()
@@ -38,16 +41,25 @@ def funzione_thread():
         dataU_list.append(dataU)
         time_list.append(timestamp)
 
-        # Mantieni solo gli ultimi 50 valori per evitare problemi di performance
-        if len(time_list) > 50:
-            dataT_list.pop(0)
-            dataU_list.pop(0)
-            time_list.pop(0)
-
         # Aggiorna il grafico: la serie ora usa time_list (numerico) che verrà formattato come orario
         dpg.set_value("temp_plot", [time_list, dataT_list])
         dpg.set_value("umi_plot", [time_list, dataU_list])
         time.sleep(10)
+        dati = []
+        with open("Dati.json") as f:
+            dati = json.load(f)
+
+        data = {
+            "Temperatura" : dataT,
+            "Umidita'" : dataU,
+            "Data e ora" : str(current_time)
+
+        }
+
+        dati.append(data)
+
+        with open("Dati.json", "w") as f:
+            json.dump(dati, f)
 
 
 # Creazione ed esecuzione del thread in background
@@ -65,7 +77,7 @@ with dpg.window(label="Dati in Tempo Reale", width=600, height=500):
     with dpg.plot(label="Andamento Temperatura e Umidità", height=300, width=580):
         dpg.add_plot_legend()
         # Imposta l'asse x in modalità "tempo" in modo da formattare i timestamp in HH:MM:SS
-        dpg.add_plot_axis(dpg.mvXAxis, label="Orario", tag="x_axis",auto_fit=True,time=True)
+        dpg.add_plot_axis(dpg.mvXAxis, label="Orario", tag="x_axis", auto_fit=True, time=True)
         dpg.add_plot_axis(dpg.mvYAxis, label="Valori", tag="y_axis")
         dpg.add_line_series([], [], label="Temperatura", parent="y_axis", tag="temp_plot")
         dpg.add_line_series([], [], label="Umidità", parent="y_axis", tag="umi_plot")
